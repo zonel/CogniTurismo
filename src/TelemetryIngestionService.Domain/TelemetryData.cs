ebc250dc067e7d7d631a82d1cc784e9df1a89b8b
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2025-02-17 23:27:19 +0100

    init

---
+++ src/TelemetryIngestionService.Domain/TelemetryData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TelemetryIngestionService.Domain
+{
+    public class TelemetryData
+    {
+        public Guid Id { get; set; }
+        public string VehicleId { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Speed { get; set; }
+        public double BatteryPercentage { get; set; }
+        public double BatteryTemperature { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
